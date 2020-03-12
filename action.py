bc2e0128f4048d0d0153ac52b1697b966200d205
Sheey <1511956322@qq.com>
2020-03-12 23:41:52 +0800

    :sparkles: support actions of comments

--- action.py
+++ action.py
@@ -1,5 +1,5 @@
 import leancloud as lc
-import json
+import json, sys
 
 config = {}
 query = None
@@ -19,9 +19,9 @@ def delete_comment(obj_id,):
     try:
         c = query.get(obj_id)
         c.destroy()
-        return True
-    except:
-        return False
+        return True, '成功！'
+    except Exception as e:
+        return False, str(e)
 
 def mark_comment(obj_id, mark_spam=False, hide=True):
     try:
@@ -34,21 +34,30 @@ def mark_comment(obj_id, mark_spam=False, hide=True):
         acl.set_public_read_access(not hide)
         c.set_acl(acl)
         c.save()
-        return True
-    except:
-        return False
+        return True, '成功！'
+    except Exception as e:
+        return False, str(e)
 
 def main(argv):
     init()
     obj_id = argv[1]
+
+    ret = False
+    msg = 'wrong arg'
+
     if argv[0] == 'delete':
-        delete_comment(obj_id)
+        ret, msg = delete_comment(obj_id)
     elif argv[0] == 'hide':
-        mark_comment(obj_id)
-    elif argv[0] == 'markAsSpam':
-        mark_comment(obj_id, True)
-    elif argv[0] == 'markAsNormal':
-        mark_comment(obj_id, False, False)
+        ret, msg = mark_comment(obj_id)
+    elif argv[0] == 'markspam':
+        ret, msg = mark_comment(obj_id, True)
+    elif argv[0] == 'marknormal':
+        ret, msg = mark_comment(obj_id, False, False)
+    print(json.dumps({
+        "code": 200 if ret else -1,
+        "msg": msg
+    }))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+
